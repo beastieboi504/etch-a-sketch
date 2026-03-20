4972099aeee25f8347472aa71f495daf6c60c0d8
beastieboi504 <beastieboi504@gmail.com>
2026-03-20 15:22:02 +0300

    Version 3, done with rainbow and earser controls

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ let colorPick = document.querySelector('#color-picker');
 let chosenColor = 'black';
 let rainbowMode = document.querySelector('#rainbow-mode');
 let isDrawing = false;
-
+let colorMode = document.querySelector('#color-mode');
+let eraser = document.querySelector('#eraser')
 let wrapper = document.querySelector('.color-picker-wrapper');
-
+let rainbowModeClicked = false;
+let colorModeClicked = false;
+let clear = document.querySelector('.clear');
 colorPick.addEventListener('input', (e) => {
     chosenColor = colorPick.value;
     wrapper.style.backgroundColor = colorPick.value;
@@ -24,13 +27,67 @@ for(let i = 0; i < 16; i++){
             square.style.backgroundColor = chosenColor;
             isDrawing = true;
         })
-        document.addEventListener('mouseup', () => isDrawing = false)
+        document.addEventListener('mouseup', () => isDrawing = false);
+        
         square.addEventListener('mouseover', (e) => {
             if(isDrawing == true){
-                e.preventDefault()
+                e.preventDefault();
                 square.style.backgroundColor = chosenColor;
             }
         });
+        colorMode.addEventListener(('click'), () => {
+            colorModeClicked = true;
+        })
+        rainbowMode.addEventListener(('click'), () => {
+            eraser.style.backgroundColor = '#202020';
+            eraser.style.color = 'rgb(62, 166, 255)';
+            rainbowMode.style.backgroundColor = 'rgb(62, 166, 255)';
+            rainbowMode.style.color = '#202020';
+
+            square.addEventListener('mousedown', (e) => {
+                let r = Math.floor(Math.random() * 255);
+                let g = Math.floor(Math.random() * 255);
+                let b = Math.floor(Math.random() * 255);
+                chosenColor = `rgb(${r}, ${g}, ${b})`;
+                e.preventDefault()
+                square.style.backgroundColor = chosenColor;
+                isDrawing = true;
+            });
+            document.addEventListener('mouseup', () => isDrawing = false);
+            square.addEventListener('mouseover', (e) => {
+            if(isDrawing == true){
+                e.preventDefault();
+                let r = Math.floor(Math.random() * 255);
+                let g = Math.floor(Math.random() * 255);
+                let b = Math.floor(Math.random() * 255);
+                chosenColor = `rgb(${r}, ${g}, ${b})`;
+                square.style.backgroundColor = chosenColor;
+                }
+
+            });
+            
+        })
+
+        eraser.addEventListener('click', () =>{
+            rainbowModeClicked = false;
+            rainbowMode.style.backgroundColor = '#202020';
+            rainbowMode.style.color = 'rgb(62, 166, 255)';
+            eraser.style.backgroundColor = 'rgb(62, 166, 255)';
+            eraser.style.color = '#202020';
+            square.addEventListener('mousedown', (e) => {
+                e.preventDefault()
+                square.style.backgroundColor = 'white';
+                isDrawing = true;
+            });
+            document.addEventListener('mouseup', () => isDrawing = false);
+            square.addEventListener('mouseover', (e) => {
+            if(isDrawing == true){
+                e.preventDefault();
+                square.style.backgroundColor = 'white';
+                }
+
+            });
+        })
     }
 }
 
@@ -44,13 +101,16 @@ gridBtn.addEventListener('click', (e) => {
             if(newGridSize == null){
                 return;
             }
-        }while(Number.isNaN(newGridSize)){
-            newGridSize = prompt('Invalid input. Enter valid Grid Dimension: ');
-            if(newGridSize == null){
-                return alert('You cancelled.')
-            }else{
-                newGridSize = Nubmer(newGridSize)
+        }if(Number.isNaN(newGridSize)){
+            while(Number.isNaN(newGridSize)){
+                newGridSize = prompt('Invalid input. Enter valid Grid Dimension: ');
+                if(newGridSize == null){
+                    return;
+                }else{
+                    newGridSize = Number(newGridSize);
+                }
             }
+
         }
     }else if(newGridSize == null){
         return alert('You cancelled.')
@@ -70,6 +130,35 @@ gridBtn.addEventListener('click', (e) => {
                 square.style.backgroundColor = chosenColor;
                 isDrawing = true;
             })
+            rainbowMode.addEventListener(('click'), () => {
+                eraser.style.backgroundColor = '#202020';
+                eraser.style.color = 'rgb(62, 166, 255)';
+                rainbowMode.style.backgroundColor = 'rgb(62, 166, 255)';
+                rainbowMode.style.color = '#202020';
+                let r = Math.floor(Math.random() * 255);
+                let g = Math.floor(Math.random() * 255);
+                let b = Math.floor(Math.random() * 255);
+                
+            });
+            eraser.addEventListener('click', () =>{
+                rainbowMode.style.backgroundColor = '#202020';
+                rainbowMode.style.color = 'rgb(62, 166, 255)';
+                eraser.style.backgroundColor = 'rgb(62, 166, 255)';
+                eraser.style.color = '#202020';
+                square.addEventListener('mousedown', (e) => {
+                    e.preventDefault()
+                    square.style.backgroundColor = 'white';
+                    isDrawing = true;
+                });
+                document.addEventListener('mouseup', () => isDrawing = false);
+                square.addEventListener('mouseover', (e) => {
+                if(isDrawing == true){
+                    e.preventDefault();
+                    square.style.backgroundColor = 'white';
+                    }
+
+                });
+            })
         document.addEventListener('mouseup', () => isDrawing = false)
         square.addEventListener('mouseover', (e) => {
             if(isDrawing == true){
@@ -80,4 +169,3 @@ gridBtn.addEventListener('click', (e) => {
         }
     }
 });
-
